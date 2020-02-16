adb6cd6fcb10d6a8bff8e00196f16d66e57e41ad
Jeffery Gontarek <jeffery@jeffsmac.local>
2020-02-16 15:57:52 -0800

    grocery app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+  const [inputValue, setInputValue] = useState("")
+  const [groceries, setGroceries] = useState([]) 
+  const listItems = groceries.map((grocery, i)=> <ul key={i}>{grocery}</ul>)
+  
+
+
+  const storeEnteredText = (e)=> {
+    setInputValue(e.target.value)
+  }
+
+  const handleButtonClick = (e)=> {
+    setGroceries([...groceries, inputValue])
+
+  
+  }
+
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2> In React</h2>
+      <h1>Grocery List</h1>
+      <input className="textInput" onChange={storeEnteredText}></input>
+      <button className="addBtn" onClick={handleButtonClick}>Add Item</button>
+      <form className="listForm">
+        <button className="listItems">{listItems}</button>
+      </form>
+    </div>
+  );
 }
 
-export default App;
+export default App;
